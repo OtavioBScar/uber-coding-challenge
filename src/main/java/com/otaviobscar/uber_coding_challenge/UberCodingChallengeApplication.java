fd496aa325651621780da55a49537fd804dadcd4
OtavioBScar <otavio.scarpelline@gmail.com>
2024-09-02 18:50:28 -0300

    initial commit :P

---
+++ src/main/java/com/otaviobscar/uber_coding_challenge/UberCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.otaviobscar.uber_coding_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberCodingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otaviobscar/uber_coding_challenge/UberCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otaviobscar.uber_coding_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberCodingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
